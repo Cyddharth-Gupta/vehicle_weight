7477f944bd54ae38e020a9269f5e2cbcccf796e4
Renuka Sharma <renukasharma592@gmail.com>
2023-10-20 16:40:37 +0530

    changed vehicle info fields

--- weight/src/Pages/VehicleInformation.jsx
+++ weight/src/Pages/VehicleInformation.jsx
@@ -29,102 +29,40 @@ const VehicleInformation = () => {
 
   const fields = [
     {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
+      name: "rfid",
+      label: "RFID",
+      type: "number",
       required: true,
       maxLength: 20,
     },
     {
-      name: "Address",
-      label: "Address",
-      type: "text",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
+      name: "vehiclenumber",
+      label: "Vehicle Number",
+      type: "number",
       required: true,
       maxLength: 20,
     },
     {
-      name: "Address",
-      label: "Address",
-      type: "text",
+      name: "vehicletype",
+      label: "Vehicle Type",
+      type: "select",
       required: true,
-      maxLength: 50,
+      options: ["ALL", "LMV", "HMV", "Pending"],
     },
     {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
+      name: "tareweight",
+      label: "Tare Weight",
+      type: "number",
       required: true,
       maxLength: 20,
-    },
-    {
-      name: "Address",
-      label: "Address",
-      type: "text",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
-      required: true,
-      maxLength: 20,
-    },
-    {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
-      required: true,
-      maxLength: 20,
-    },
-    {
-      name: "Address",
-      label: "Address",
-      type: "text",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
-      required: true,
-      maxLength: 20,
-    },
-    {
-      name: "Address",
-      label: "Address",
-      type: "text",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      name: "ZoneName",
-      label: "Zone Name",
-      type: "text",
-      required: true,
-      maxLength: 20,
-    },
-    {
-      name: "Address",
-      label: "Address",
-      type: "text",
-      required: true,
-      maxLength: 50,
     },
   ];
 
-  const customClass = "grid w-2/3 grid-cols-2 gap-4 ";
-  const customInputClass = "mx-14";
-  const customLabelClass = "mx-14";
-  const customButtonClass="px-32 my-5";
+  const customInputClass = "mx-14 mb-10";
+  const customSelectClass = "mx-14 mb-10";
+  const customLabelClass = "mx-14 ";
+  const customButtonClass="px-28 my-5";
+  const customClass = "ml-60";
   return (
     <div className="flex flex-row">
       <NavigationDrawer />
@@ -140,17 +78,19 @@ const VehicleInformation = () => {
         </div>
 
         <main className="flex flex-row justify-normal ">
-          <img src={truck} alt="truck" className="-ml-60" />
+          <img src={truck} alt="truck" className="-ml-70" />
           <ReusableForm
             onSubmit={onSubmit}
             className="flex-row"
             fields={fields}
             errors={errors}
+
             submitButtonLabel={"Submit"}
-            customClass={customClass}
             customInputClass={customInputClass}
             customLabelClass={customLabelClass}
             customButtonClass={customButtonClass}
+            customSelectClass = {customSelectClass}
+            customClass={customClass}
           />
         </main>
       </div>
