984510a237581a5107ff255d3aa6733e9cd21cd2
Renuka Sharma <renukasharma592@gmail.com>
2023-10-20 23:04:03 +0530

    added pagination

--- weight/src/Components/UserTrackerTable.jsx
+++ weight/src/Components/UserTrackerTable.jsx
@@ -242,7 +242,7 @@ export default function UserTrackerTable() {
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
         pageSizeOptions={[5, 10]}
--- weight/src/Components/ZoneTrackerTable.jsx
+++ weight/src/Components/ZoneTrackerTable.jsx
@@ -117,7 +117,7 @@ export default function ZoneTrackerTable() {
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
         pageSizeOptions={[5, 10]}
