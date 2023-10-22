bf82a0ef8cb48b572ce8354b15107b5a049d1f12
Renuka Sharma <renukasharma592@gmail.com>
2023-10-22 21:09:15 +0530

    added video snackbar and other css

---
+++ weight/src/Components/DataHandler.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const DataHandler = ({ selectedFiles }) => {
+  const handleDataAndSubmit = () => {
+    if (selectedFiles.length === 0) {
+      return;
+    }
+
+    const uploadedFiles = [];
+    const totalFiles = selectedFiles.length;
+
+    selectedFiles.forEach((file) => {
+      if (file.type === "application/json") {
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+          try {
+            const jsonData = JSON.parse(event.target.result);
+
+            // Process jsonData as needed
+            console.log(jsonData);
+
+            // Add the file to the uploaded files
+            uploadedFiles.push({ file, data: jsonData });
+
+            if (uploadedFiles.length === totalFiles) {
+              // All JSON files have been processed
+              // Call your API to submit the data
+            }
+          } catch (error) {
+            console.error("Error parsing JSON file:", error);
+          }
+        };
+
+        reader.readAsText(file);
+      }
+    });
+  };
+
+  // Function to parse a JSON file and return data as a Promise
+  const parseJSONFile = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        try {
+          const jsonData = JSON.parse(e.target.result);
+          resolve(jsonData);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      reader.readAsText(file);
+    });
+  };
+
+  // Use useEffect to trigger data handling when selectedFiles change
+  useEffect(() => {
+    if (selectedFiles.length > 0) {
+      handleDataAndSubmit();
+    }
+  }, [selectedFiles]);
+
+  return null;
+};
+
+export default DataHandler;
